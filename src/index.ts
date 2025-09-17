4e2795d436fc1897ee765e91e4316b25dd0dc7e4
daixu <daixu@xiaomi.com>
2025-09-17 10:04:16 +0800

    feat: adapt to new version

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,135 @@
+/**
+ * Cloudbase Adapter for QuickApp
+ */
+
 import {
-  AbstractSDKRequest,
+  SDKRequestInterface,
   IRequestOptions,
   IUploadRequestOptions,
   StorageInterface,
   WebSocketInterface,
   WebSocketContructor,
   SDKAdapterInterface,
-  formatUrl
-} from '@cloudbase/adapter-interface';
+  formatUrl,
+} from '@cloudbase/adapter-interface'
 
 // isMatch函数判断当前平台是否匹配
 function isMatch(): boolean {
   try {
-    const app = require('@system.app');
-    const info = app.getInfo();
-    return !!info;
+    const app = require('@system.app')
+    const info = app.getInfo()
+    return !!info
   }
   catch (e) {
-    return false;
+    console.log('isMatch error: ', e)
+    return false
   }
 }
 
-// Request类为平台特有的网络请求，必须实现post/upload/download三个public接口
-export class QappRequest extends AbstractSDKRequest {
-  // 实现post接口
+// Request类为平台特有的网络请求，必须实现post/get/fetch/upload/download 五个public接口
+export class QAppRequest implements SDKRequestInterface {
   public post(options: IRequestOptions) {
     return new Promise((resolve, reject) => {
       try {
-        const fetch = require('@system.fetch');
+        const fetch = require('@system.fetch')
         const {
           url,
           data,
           headers
-        } = options;
+        } = options
 
         fetch.fetch({
           url: formatUrl('https:', url),
-          data: JSON.stringify(data), // fixed quickapp post fetch isn't support 'Content-Type' when post json object
+          data: JSON.stringify(data),
           method: 'POST',
           header: headers,
-          responseType: 'json', // set reponseType is 'json', default 'text'
+          responseType: 'json',
           success: function (res) {
-            res.statusCode = res.code; // fixed res.status is undefined in tcb-js-sdk
+            res.statusCode = res.code
 
-            resolve(res);
+            resolve(res)
           },
           fail: function (err) {
-            reject(err);
+            reject(err)
           }
-        });
+        })
       } catch(e) {
-        reject(e);
+        reject(e)
       }
-    });
+    })
   }
-  // 实现upload接口
+
+  public get(options: IRequestOptions) {
+    return new Promise((resolve, reject) => {
+      try {
+        const fetch = require('@system.fetch')
+        const {
+          url,
+          data,
+          headers
+        } = options
+
+        fetch.fetch({
+          url: formatUrl('https:', url),
+          data: JSON.stringify(data),
+          method: 'GET',
+          header: headers,
+          responseType: 'json',
+          success: function (res) {
+            res.statusCode = res.code
+
+            resolve(res)
+          },
+          fail: function (err) {
+            reject(err)
+          }
+        })
+      } catch(e) {
+        reject(e)
+      }
+    })
+  }
+
+  public fetch(options: IRequestOptions) {
+    return new Promise((resolve, reject) => {
+      try {
+        const fetch = require('@system.fetch')
+        const {
+          url,
+          data,
+          body,
+          headers,
+          method
+        } = options
+
+        fetch.fetch({
+          url: formatUrl('https:', url),
+          data: data ? JSON.stringify(data) : body,
+          method,
+          header: headers,
+          responseType: 'json',
+          success: function (res) {
+            res.statusCode = res.code
+
+            resolve(res)
+          },
+          fail: function (err) {
+            reject(err)
+          }
+        })
+      } catch(e) {
+        reject(e)
+      }
+    })
+  }
+
   public upload(options: IUploadRequestOptions) {
     return new Promise((resolve, reject) => {
       try {
-        const request = require('@system.request');
-        const { url, data, file, headers } = options;
+        const request = require('@system.request')
+        const { url, data, file, headers } = options
         // format data
-        let dlist = [];
+        let dlist = []
         Object.keys(data).forEach(i => {
           dlist.push({name: i, value: data[i]})
         })
@@ -86,29 +154,30 @@ export class QappRequest extends AbstractSDKRequest {
             const result = {
               statusCode: res.code,
               data: res.data || {}
-            };
+            }
 
             // 200转化为201
             if (res.code === 200 && data.success_action_status) {
-              result.statusCode = parseInt(data.success_action_status, 10);
+              result.statusCode = parseInt(data.success_action_status, 10)
             }
-            resolve(result);
+            resolve(result)
           },
           fail: function(data, code) {
-            reject({ data, code });
+            reject({ data, code })
           }
-        });
+        })
       } catch(e) {
-        reject(e);
+        reject(e)
       }
-    });
+    })
   }
+
   // 实现download接口
   public download(options: IRequestOptions) {
     return new Promise((resolve, reject) => {
       try {
-        const request = require('@system.request');
-        const { url, headers } = options;
+        const request = require('@system.request')
+        const { url, headers } = options
 
         request.download({
           url: formatUrl('https:', url),
@@ -119,19 +188,131 @@ export class QappRequest extends AbstractSDKRequest {
               resolve({
                 statusCode: 200,
                 tempFilePath: url
-              });
+              })
             } else {
-              resolve(res);
+              resolve(res)
             }
           },
           fail: function(data, code) {
-            reject(JSON.stringify({ data, code }));
+            reject(JSON.stringify({ data, code }))
           }
-        });
+        })
       } catch(e) {
-        reject(e);
+        reject(e)
       }
-    });
+    })
+  }
+}
+
+// auth模块网络请求
+export const requestFunction = function (url, options) {
+  return new Promise((resolve, reject) => {
+    try {
+      const fetch = require('@system.fetch')
+      const {
+        data,
+        body,
+        headers,
+        method
+      } = options
+
+      fetch.fetch({
+        url: formatUrl('https:', url),
+        data: JSON.stringify(body),
+        method,
+        header: headers,
+        responseType: 'json',
+        success: function (res) {
+          res.statusCode = res.code
+          resolve(res.data)
+        },
+        fail: function (err) {
+          reject(err)
+        }
+      })
+    } catch(e) {
+      reject(e)
+    }
+  })
+}
+
+// Storage为平台特有的本地存储，必须实现setItem/getItem/removeItem/clear四个接口
+export const QappStorage: StorageInterface = {
+  setItem(key: string, value: any) {
+    return new Promise((resolve, reject) => {
+      try {
+        const storage = require('@system.storage')
+        storage.set({
+          key,
+          value,
+          success: function(data) {
+            resolve(data)
+          },
+          fail: function(data, code) {
+            // reject({ data, code })
+            reject(JSON.stringify({ data, code }))
+          }
+        })
+      } catch(e) {
+        reject(e)
+      }
+    })
+  },
+
+  getItem(key: string): any {
+      return new Promise((resolve, reject) => {
+        try {
+          const storage = require('@system.storage')
+          storage.get({
+            key,
+            success: function(data) {
+              resolve(data)
+            },
+            fail: function(data, code) {
+              reject(JSON.stringify({ data, code }))
+            }
+          })
+        } catch(e) {
+          reject(e)
+        }
+      })
+  },
+
+  removeItem(key: string) {
+    return new Promise((resolve, reject) => {
+      try {
+        const storage = require('@system.storage')
+        storage.delete({
+          key,
+          success: function(data) {
+            resolve(data)
+          },
+          fail: function(data, code) {
+            reject(JSON.stringify({ data, code }))
+          }
+        })
+      } catch(e) {
+        reject(e)
+      }
+    })
+  },
+
+  clear() {
+    return new Promise((resolve, reject) => {
+      try {
+        const storage = require('@system.storage')
+        storage.clear({
+          success: function(data) {
+            resolve(data)
+          },
+          fail: function(data, code) {
+            reject(JSON.stringify({ data, code }))
+          }
+        })
+      } catch(e) {
+        reject(e)
+      }
+    })
   }
 }
 
@@ -139,68 +320,76 @@ export class QappRequest extends AbstractSDKRequest {
 export class QappWebSocket {
   constructor(url: string, options: object = {}) {
     try {
-      const websocketfactory = require('@system.websocketfactory');
+      const websocketfactory = require('@system.websocketfactory')
       let ws = websocketfactory.create({
         url,
         ...options
-      });
+      })
 
       const socketTask: WebSocketInterface = {
         set onopen(cb) {
-          ws.onopen = cb;
+          ws.onopen = cb
         },
         set onmessage(cb) {
-          ws.onmessage = cb;
+          ws.onmessage = cb
         },
         set onclose(cb) {
-          ws.onclose = cb;
+          ws.onclose = cb
         },
         set onerror(cb) {
-          ws.onerror = cb;
+          ws.onerror = cb
         },
         send: (data) => {
-          return ws.send({ data });
+          return ws.send({ data })
         },
         close: (code? : number, reason? : string) => {
           return ws.close({
             code: code,
             reason: reason
-          });
+          })
         },
         get readyState() {
-          return ws.readyState;
+          return ws.readyState
         },
         CONNECTING: 0,
         OPEN: 1,
         CLOSING: 2,
         CLOSED: 3
-      };
-      return socketTask;
+      }
+      return socketTask
     } catch (e) {}
   }
 }
 
 // genAdapter函数创建adapter实体
-function genAdapter() {
+// options 为 cloudbase.useAdapters(adapter, options) 时传入的参数
+function genAdapter(options) {
   const adapter: SDKAdapterInterface = {
+    /* ts-ignore */
     root: global,
-    reqClass: QappRequest,
+    reqClass: QAppRequest,
     wsClass: QappWebSocket as WebSocketContructor,
+    // 获取平台唯一应用标识的api，微信小程序返回的是appid，快应用没有配置appid的概念，所以返回packageName为平台唯一标识
+    // 查了代码，发现没有用到这个函数
     getAppSign: function () {
       try {
         const app = require('@system.app')
-        return app.getInfo().packageName;
-      } catch(e) {}
+        return app.getInfo().packageName
+      } catch(e) {
+        console.log('getAppSign: ', e)
+        return ''
+      }
     }
-  };
-  return adapter;
+  }
+  return adapter
 }
 
+// 三者缺一不可
 const adapter = {
   genAdapter,
   isMatch,
-  // runtime为标记平台唯一性的说明
-  runtime: 'quickapp'
-};
+  // runtime标记平台唯一性
+  runtime: 'quickapp_android',
+}
 
-export default adapter;
+export default adapter
